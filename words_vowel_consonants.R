a7f591e456b7bbb648456e80cf91e795b72033f8
Rachel Lobay <rachel.lobay@stat.ubc.ca>
2018-11-29 11:45:29 -0800

    Adding comments to vowels and consonants.

--- words_vowel_consonants.R
+++ words_vowel_consonants.R
@@ -1,8 +1,8 @@
 # words vowels consonants table for makefile
 words <- readLines("words.txt")
 
-vowels = stringr::str_count(words, "[aeiou]")
-consonants = stringr::str_count(words, "[^aeiou]")
+vowels = stringr::str_count(words, "[aeiou]") # find vowels
+consonants = stringr::str_count(words, "[^aeiou]") # find consonants
 
 count_vowels <- table(vowels) # how many words with 1,2,3,... etc. vowels
 count_consonants <- table(consonants) # how many words with 1,2,3,... etc. consonants
